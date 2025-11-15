dcc22ff80a2f5f23a0dad2afcf990522a21541bc
L-alam <lalam@bu.edu>
2025-11-14 21:01:05 -0500

    backend setup

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/second', (req, res) => {
+    res.send('this is the second screen')
+})
+
 app.listen(port, () => {
   console.log(`Example app not listening on port ${port}`)
 })
